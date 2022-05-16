4ca39b7b6f45adb154887667872d0e76abed6eeb
GreenRoot <1.ooo.ooo@bk.ru>
2022-05-16 21:48:03 +0300

    added rules for: queen, rook and bishop

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -12,7 +12,7 @@ const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
         <div 
             className={['cell', cell.color, selected ? 'selected' : ''].join(' ')}
             onClick = {() => click(cell)}
-            style = {{filter: cell.avaliable && cell.figure ? 'sepia(1)' : ''}}
+            style = {{background: cell.avaliable && cell.figure ? '#5d5341' : ''}}
         >
             {cell.avaliable && !cell.figure && <div className={'available'} />}
             {cell.figure?.logo && <img src={cell.figure.logo} alt=''/>}
